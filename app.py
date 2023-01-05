8c56fbfed68f0d5242f5d0f70b25b94bb0f39e6b
Leandro Damascena <leandro.damascena@gmail.com>
2023-01-05 12:39:51 +0000

    Initial commit

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 logger = Logger(log_uncaught_exceptions=True)
 
 if __name__ == "__main__":
-    raise(Exception("This will produce a structured log"))
+    raise(Exception("This will create a structured log"))
